947f2b4e3806983cd2d73445b307e6ae4bad86f5
amoweolubusayo <amoweolubusayo@gmail.com>
2019-01-17 11:45:12 +0100

    Update index.php

--- testcase/index.php
+++ testcase/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>PHP Mysql REST API CRUD</title>
+		<title>REST API EXERCISE</title>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
